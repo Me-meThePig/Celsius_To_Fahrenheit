b57cfa0ac26bedcd6cbe98291c2c411ad0d723fb
Me-me The Pig <abdelkefi08@gmail.com>
2025-01-18 22:08:03 -0500

    Cleaned up code
    - Removed useless import
    - Added more comments
    - Rearranged a variable

--- TempConv/main.py
+++ TempConv/main.py
@@ -1,38 +1,42 @@
 from tkinter import *
-import tkinter as tk
 from tkinter import IntVar
 
 
 def on_change(*args):
     try:
         celsius = textc.get()
-        # celsius to fahrenheit
+        # convert Celsius to Fahrenheit
         fahrenheit = (celsius * 1.8) + 32
         fntL.config(text=f"Fahrenheit: {fahrenheit:.2f}")
     except TclError:
-        # empty or invalid input
+        # handle empty or invalid input
         fntL.config(text="Fahrenheit: Invalid Input")
 
 
+# app setup
 root = Tk()
 root.title("Temperature Converter")
 root.geometry("300x100")
 root.resizable(False, False)
 
-textc = IntVar(root, name="int")
 
 # Celsius
+textc = IntVar(root, name="int")
+
 clsL = Label(root, text="Celsius:", font="Arial 12 bold")
 clsL.place(x=5, y=10)
 
 clsE = Entry(root, textvariable=textc, font=30, width=25)
 clsE.place(x=5, y=40)
 
+
 # Fahrenheit
 fntL = Label(root, text=f"Fahrenheit: {textc.get()}", font="Arial 12 bold")
 fntL.place(x=5, y=70)
 
+# update Fahrenheit text
 textc.trace_add("write", on_change)
 on_change()
+
 
 root.mainloop()
